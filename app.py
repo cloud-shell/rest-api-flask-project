fcdfdd1b83a6baecd402b34ee53df30b6450cbe7
cloud <helloeverybody@gmail.com>
2022-11-30 13:13:06 +0900

    flask-smorest-sqlalchemy

--- app.py
+++ app.py
@@ -1,54 +1,36 @@
-from flask import Flask, request
+import uuid
+import os
+from flask import Flask
+from flask_smorest import Api
 
-app = Flask(__name__)
+from db import db
+import models
 
-stores = [
-    {
-        "name": "My Store",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-        ]        
-    }
-]
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
 
-@app.get("/store")
-def get_stores():
-    return {"stores": stores}
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-@app.post("/store")
-def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items":[]}
-    stores.append(new_store)
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db" #os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    db.init_app(app)
 
-    return new_store, 201
+    api = Api(app)
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-        
-    return {"message": "Store not found"}, 404
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-        
-    return {"message": "Store not found"}, 404
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
 
-@app.get("/store/<string:name>/item")
-def get_item_in_store(name):
-    for store in stores:
-        if store["name"] == name:            
-            return {"items": store["items"]}
-        
-    return {"message": "Store not found"}, 404
+    return app
